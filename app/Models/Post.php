e9b956d7eb3c22132543d67e6815f2448b5c41e4
Habtesellasie Fissha <havecodesoft@gmail.com>
2024-03-09 02:19:37 +0300

    category & post pages on progress

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    // a post belongs to many categories
+    // and a category belogs to many posts
+}
